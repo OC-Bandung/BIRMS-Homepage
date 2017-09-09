2cf6e43ae9bbf6c1ee69db5e855650c04e7cc177
dc-coder <mir.mir@gmail.com>
2017-09-09 19:10:54 +0700

    basic map layout

--- js/map.js
+++ js/map.js
@@ -1,118 +1,116 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibWlyZWlsbGUiLCJhIjoiY2oyMXhxeHEwMDAwODMzbWdmc2pkZzc2MCJ9.dnTlkXT6i4qEy7LygdIhfA';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mireille/cj7bzd3zqal562smk2c0cqpyz',
-    center: [107.613285,  -6.917759],
-    maxZoom: 20,
-    minZoom: 10,
-    zoom: 13
+var map;
+      
+      function initMap() {
+        
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: {lat: -6.916768, lng: 107.619056}
+        });
 
-});
+        map.setOptions({styles: styles['silver']});
 
-var title = document.getElementById('location-title');
-var description = document.getElementById('location-description');
+        
 
-var locations = [{
-    "id": "2",
-    "title": "The Bronx",
-    "description": "This is where hip-hop was born, where the Yankees became a dynasty and where you can find New York City's leading zoo and botanical garden.",
-    "camera": {
-        center: [107.609246, -6.911553,],
-        zoom: 13,
-        pitch: 50
-    }
-}, {
-    "id": "3",
-    "title": "Brooklyn",
-    "description": "No matter how hip it looks on TV, NYC's most populous borough is best experienced in person. Read on to find out about live music, Prospect Park, Nets basketball and more.",
-    "camera": {
-        center: [107.584697, -6.911490],
-        bearing: -8.9,
-        zoom: 14
-    }
-}, {
-    "id": "1",
-    "title": "Manhattan",
-    "description": "Even if you think you know Manhattan—its world-class museums, fine dining and unforgettable views—the borough always has something new and exciting in store.",
-    "camera": {
-        center: [107.594160, -6.921895],
-        bearing: 25.3,
-        zoom: 13
-    }
-}, {
-    "id": "4",
-    "title": "Queens",
-    "description": "Taste food from around the globe, watch Mets baseball and US Open tennis, see cutting-edge art and more in one of the world's most diverse places.",
-    "camera": {
-        center: [107.604466, -6.933023],
-        bearing: 36,
-        zoom: 15
-    }
-}, {
-    "id": "5",
-    "title": "Staten Island",
-    "description": "Take a free ferry ride to an island getaway filled with historic architecture, stunning views, gardens and many family-friendly attractions.",
-    "camera": {
-        center: [107.620938,-6.928305],
-        bearing: 28.4,
-        zoom: 14
-    }
-}, {
-    "title": "Boroughs of new york",
-    "description": "New York City is made up of five boroughs: the Bronx, Brooklyn, Manhattan, Queens and Staten Island. Each one has enough attractions—and enough personality—to be a city all its own.",
-    "camera": {
-        center: [ 107.622808,-6.912025],
-        zoom: 12,
-        bearing: 0,
-        pitch: 0
-    }
-}];
+        // NOTE: This uses cross-domain XHR, and may not work on older browsers.
+        map.data.loadGeoJson('geojson/kota-bandung-level-kecamatan.json');
 
-function highlightBorough(code) {
-    // Only show the polygon feature that cooresponds to `borocode` in the data
-    map.setFilter('highlight', ["==", "borocode", code]);
-}
+        map.data.setStyle({
+          fillColor: green,
+          strokeWeight: 1
+        });
 
-function playback(index) {
-    title.textContent = locations[index].title;
-    description.textContent = locations[index].description;
+      }
 
-    highlightBorough(locations[index].id ? locations[index].id : '');
 
-    // Animate the map position based on camera properties
-    map.flyTo(locations[index].camera);
+var styles = {
+        default: null,
+        silver: [
 
-    map.once('moveend', function() {
-        // Duration the slide is on screen after interaction
-        window.setTimeout(function() {
-            // Increment index
-            index = (index + 1 === locations.length) ? 0 : index + 1;
-            playback(index);
-        }, 3000); // After callback, show the location for 3 seconds.
-    });
-}
-
-// Display the last title/description first
-title.textContent = locations[locations.length - 1].title;
-description.textContent = locations[locations.length - 1].description;
-
-map.on('load', function() {
-
-    map.addLayer({
-        "id": "highlight",
-        "type": "fill",
-        "source": {
-            "type": "vector",
-            "url": "mapbox://mapbox.8ibmsn6u"
-        },
-        "source-layer": "original",
-        "paint": {
-            "fill-color": "#fd6b50",
-            "fill-opacity": 0.25
-        },
-        "filter": ["==", "borocode", ""]
-    }, 'neighborhood_small_label'); // Place polygon under the neighborhood labels.
-
-    // Start the playback animation for each borough
-   ///playback(0);
-});
+          {
+            elementType: 'geometry',
+            stylers: [{color: '#f5f5f5'}]
+          },
+          {
+            elementType: 'labels.icon',
+            stylers: [{visibility: 'off'}]
+          },
+          {
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#616161'}]
+          },
+          {
+            elementType: 'labels.text.stroke',
+            stylers: [{color: '#f5f5f5'}]
+          },
+           
+          {
+            featureType: 'administrative.land_parcel',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#bdbdbd'}]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [{color: '#eeeeee'}]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#757575'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{color: '#e5e5e5'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#9e9e9e'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{color: '#ffffff'}]
+          },
+          {
+            featureType: 'road.arterial',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#757575'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{color: '#dadada'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#616161'}]
+          },
+          {
+            featureType: 'road.local',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#9e9e9e'}]
+          },
+          {
+            featureType: 'transit.line',
+            elementType: 'geometry',
+            stylers: [{color: '#e5e5e5'}]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'geometry',
+            stylers: [{color: '#eeeeee'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{color: '#c9c9c9'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#9e9e9e'}]
+          }
+        ] } ;
