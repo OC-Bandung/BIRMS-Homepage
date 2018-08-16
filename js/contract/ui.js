8e8a9da9ef8f2a57a1d6197b96bb94b016d1ba83
dc-coder <mir.mir@gmail.com>
2018-08-16 18:41:06 -0400

    adding feedback and docs

--- js/contract/ui.js
+++ js/contract/ui.js
@@ -1,3 +1,13 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$("#bdg-feedback-form").click(function() {
+  var lnk = "https://docs.google.com/forms/d/e/1FAIpQLScs-1715tHTn60Ncj85_1zW7TQg8vzTWjwajCJCEdBGEmSfJw/viewform?usp=pp_url";
+  lnk += "&entry.1755301375=" + $("#ocid").text();
+  lnk += "&entry.788440630=" +   $("#page-title").text() ;
+  lnk += "&entry.1796765524= ";
+  lnk += "&entry.439080160= ";
+  lnk += "&entry.831032170= ";
+  $(this).attr("href", lnk);
+});
